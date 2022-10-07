11b594e387b72b0dc1327a888b460d892b422113
Michal Dziadkowiec <michaldziadkowiec@yahoo.pl>
2022-10-07 12:14:46 +0200

    feat(auth): connect login form with API

--- frontend/src/features/auth/components/LoginForm.tsx
+++ frontend/src/features/auth/components/LoginForm.tsx
@@ -1,28 +1,59 @@
+import axios from 'axios';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router';
 import { Button, Form } from 'semantic-ui-react';
 
+type InputFormFields = {
+  email: string;
+  password: string;
+};
+
 export const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    // formState: { errors },
+  } = useForm<InputFormFields>();
+  const navigate = useNavigate();
+  const onSubmit = handleSubmit(async ({ email, password }) => {
+    const response = await axios({
+      url: '/api/user/login',
+      method: 'post',
+      data: {
+        email,
+        password,
+      },
+    });
+
+    if (response.data.authToken) {
+      console.log('response.data.authToken', response.data.authToken);
+
+      navigate('/app');
+    }
+  });
+
   return (
-    <Form>
+    <Form onSubmit={onSubmit}>
       <Form.Field>
         <input
           type='email'
-          name='email'
           placeholder='Email'
           aria-label='Email'
           data-testid='login-form-email-field'
+          {...register('email')}
         />
       </Form.Field>
       <Form.Field>
         <input
           type='password'
-          name='password'
           placeholder='Password'
           aria-label='Password'
           data-testid='login-form-password-field'
+          {...register('password')}
         />
       </Form.Field>
-      <Button primary data-testid='login-form-submit-button'>
+      <Button type='submit' primary data-testid='login-form-submit-button'>
         Log in
       </Button>
     </Form>
