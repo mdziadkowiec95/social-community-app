97398010a2cef24064837e81b834e19698185c7b
Michal Dziadkowiec <michaldziadkowiec@yahoo.pl>
2022-10-12 12:48:55 +0200

    chore(frontend-app): add absolute imports

--- frontend/src/pages/non-auth/NonAuthRoot.tsx
+++ frontend/src/pages/non-auth/NonAuthRoot.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
-import { NavMenu } from '../../features/navigation/components/NavMenu/NavMenu';
+import { NavMenu } from 'features/navigation/components/NavMenu/NavMenu';
 
 export const NonAuthRoot = () => {
   fetch('/api/user/login', {
